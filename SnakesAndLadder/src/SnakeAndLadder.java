28f684514d15d2969cb30080db24c4feebc8037a
dongyoung <ehddmleheheh@naver.com>
2019-05-19 13:24:20 +0900

    update

--- SnakesAndLadder/src/SnakeAndLadder.java
+++ SnakesAndLadder/src/SnakeAndLadder.java
@@ -1,15 +1,24 @@
-import java.util.LinkedList; 
-import java.util.Queue; 
+import java.util.*;
   
 public class SnakeAndLadder  
 { 
-    // An entry in queue used in BFS 
-    static class qentry  
-    { 
-        int v;// Vertex number 
-        int dist;// Distance of this vertex from source 
-    } 
-  
+
+    static class qentry implements Comparable<qentry>
+    {
+    	int v;
+    	int dist;
+    	
+    	@Override
+    	public int compareTo(qentry target)
+    	{
+    		if(this.v < target.v)
+    			return 1;
+    		else if(this.v > target.v)
+    			return -1;
+    		return 0;			
+    	}
+
+    }
     // This function returns minimum number of dice  
     // throws required to Reach last cell from 0'th cell  
     // in a snake and ladder game. move[] is an array of  
@@ -17,36 +26,38 @@ public class SnakeAndLadder
     // is no snake or ladder from cell i, then move[i]  
     // is -1 Otherwise move[i] contains cell to which  
     // snake or ladder at i takes to. 
-    static int getMinDiceThrows(int move[], int n)  
-    { 
-        int visited[] = new int[n]; 
-        Queue<qentry> q = new LinkedList<>(); 
+    static int getMinDiceThrows(int move[], int n)  // find min dice
+    { // move = map / n = array MAX 
+    	int count = 0;
+        int visited[] = new int[n];  // visited array
+        PriorityQueue<qentry> q = new PriorityQueue<>();  // queue
         qentry qe = new qentry(); 
         qe.v = 0; 
         qe.dist = 0; 
   
         // Mark the node 0 as visited and enqueue it. 
         visited[0] = 1; 
-        q.add(qe); 
+        q.offer(qe); // init
   
         // Do a BFS starting from vertex at index 0 
         while (!q.isEmpty())  
         { 
-            qe = q.remove(); 
+            qe = q.poll();
+            System.out.println("remove : " + qe.dist+ "," + qe.v);
             int v = qe.v; 
   
             // If front vertex is the destination  
             // vertex, we are done 
-            if (v == n - 1) 
+            if (v == n - 1)  // goal
                 break; 
   
             // Otherwise dequeue the front vertex and  
             // enqueue its adjacent vertices (or cell  
             // numbers reachable through a dice throw) 
-            for (int j = v + 1; j <= (v + 6) && j < n; ++j)  
+            for (int j = v + 1; j <= (v + 6) && j < n; ++j)  //dice = 1 ~ 6 && j's max = n-1
             { 
                 // If this cell is already visited, then ignore 
-                if (visited[j] == 0) 
+                if (visited[j] == 0)  // didn't visit ary
                 { 
                     // Otherwise calculate its distance and  
                     // mark it as visited 
@@ -57,15 +68,22 @@ public class SnakeAndLadder
                     // Check if there a snake or ladder at 'j' 
                     // then tail of snake or top of ladder 
                     // become the adjacent of 'i' 
-                    if (move[j] != -1) 
+                    if (move[j] != -1)  // first visit
                         a.v = move[j]; 
-                    else
+                    else // already visited
                         a.v = j; 
-                    q.add(a); 
+                    q.offer(a); 
                 } 
             } 
+            System.out.println("===Queue print===");
+            for(qentry data : q)
+            {
+            	System.out.print( "v = " + data.v + ",dist = "+ data.dist + "/");
+            }
+            System.out.println();
         } 
-  
+        //list 추가한거 출력해보기
+        //print visited
         // We reach here when 'qe' has last vertex 
         // return the distance of vertex in 'qe' 
         return qe.dist; 
@@ -74,19 +92,19 @@ public class SnakeAndLadder
     public static void main(String[] args)  
     { 
         // Let us construct the board given in above diagram 
-        int N = 30; 
+        int N = 36; 
         int moves[] = new int[N]; 
-        for (int i = 0; i < N; i++) 
+        for (int i = 0; i < N; i++)  // init ary to -1
             moves[i] = -1; 
   
         // Ladders 
-        moves[2] = 15; 
-        moves[14] = 24; 
-        moves[20] = 31; 
+        moves[3] = 15; 
+        moves[15] = 24; 
+        moves[21] = 31; 
   
         // Snakes 
-        moves[11] = 1; 
-        moves[28] = 3; 
+        moves[12] = 1; 
+        moves[29] = 3; 
         moves[34] = 21;  
   
         System.out.println("Min Dice throws required is " +  
---
+++ SnakesAndLadder/src/qentry.java
@@ -0,0 +1,11 @@
+
+public class qentry {
+    public qentry(int v, int dist) {
+		// TODO Auto-generated constructor stub
+    	this.v = v;
+    	this.dist = dist;
+	}
+	// An entry in queue used in BFS 
+    int v;// Vertex number 
+    int dist;// Distance of this vertex from source \
+}
