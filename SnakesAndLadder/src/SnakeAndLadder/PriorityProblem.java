18b84cb6c4bfb8e2ddc52191d7cdc693cf902bad
dongyyyyy <ehddmleheheh@naver.com>
2019-06-04 15:32:04 +0900

    update

--- SnakesAndLadder/src/SnakeAndLadder/PriorityProblem.java
+++ SnakesAndLadder/src/SnakeAndLadder/PriorityProblem.java
@@ -1,7 +1,7 @@
 package SnakeAndLadder;
 import java.util.*;
  /* 우선순위 우선 탐색 */
-public class SnakeAndLadder  
+public class PriorityProblem  
 { 
 
     static class qentry implements Comparable<qentry>
@@ -12,21 +12,13 @@ public class SnakeAndLadder
     	@Override
     	public int compareTo(qentry target)
     	{
-    		if(this.v < target.v)
+    		if(this.dist > target.dist && this.v < target.v )
     			return 1;
-    		else if(this.v > target.v)
-    			return -1;
-    		return 0;			
+    		else 
+    			return -1;	
     	}
 
     }
-    // This function returns minimum number of dice  
-    // throws required to Reach last cell from 0'th cell  
-    // in a snake and ladder game. move[] is an array of  
-    // size N where N is no. of cells on board If there  
-    // is no snake or ladder from cell i, then move[i]  
-    // is -1 Otherwise move[i] contains cell to which  
-    // snake or ladder at i takes to. 
     static int getMinDiceThrows(int move[], int n)  // find min dice
     { // move = map / n = array MAX 
     	int count = 0;
@@ -35,55 +27,34 @@ public class SnakeAndLadder
         qentry qe = new qentry(); 
         qe.v = 0; 
         qe.dist = 0; 
-  
-        // Mark the node 0 as visited and enqueue it. 
         visited[0] = 1; 
         q.offer(qe); // init
-  
-        // Do a BFS starting from vertex at index 0 
         while (!q.isEmpty())  
         { 
             qe = q.poll();
             System.out.println("remove : " + qe.dist+ "," + qe.v);
             int v = qe.v; 
-  
-            // If front vertex is the destination  
-            // vertex, we are done 
             if (v == n - 1)  // goal
                 break; 
-  
-            // Otherwise dequeue the front vertex and  
-            // enqueue its adjacent vertices (or cell  
-            // numbers reachable through a dice throw) 
             for (int j = v + 1; j <= (v + 6) && j < n; ++j)  //dice = 1 ~ 6 && j's max = n-1
             { 
                 // If this cell is already visited, then ignore 
                 if (visited[j] == 0)  // didn't visit ary
                 { 
-                    // Otherwise calculate its distance and  
-                    // mark it as visited 
                     qentry a = new qentry(); 
                     a.dist = (qe.dist + 1); 
                     visited[j] = 1; 
-  
-                    // Check if there a snake or ladder at 'j' 
-                    // then tail of snake or top of ladder 
-                    // become the adjacent of 'i' 
+                    count++;
                     if (move[j] != -1)  // first visit
                         a.v = move[j]; 
                     else // already visited
                         a.v = j; 
                     q.offer(a); 
-                    count++;
                 } 
             } 
            // System.out.println("===Queue print===");
            // System.out.println();
         } 
-        //list 추가한거 출력해보기
-        //print visited
-        // We reach here when 'qe' has last vertex 
-        // return the distance of vertex in 'qe'
         System.out.println("count = " + count);
         return qe.dist; 
     } 
--- SnakesAndLadder/src/SnakeAndLadder/StackProblem.java
+++ SnakesAndLadder/src/SnakeAndLadder/StackProblem.java
@@ -3,14 +3,7 @@ import java.util.*;
 
  /* 너비 우선 탐색 */
 public class StackProblem  
-{ 
-    // This function returns minimum number of dice  
-    // throws required to Reach last cell from 0'th cell  
-    // in a snake and ladder game. move[] is an array of  
-    // size N where N is no. of cells on board If there  
-    // is no snake or ladder from cell i, then move[i]  
-    // is -1 Otherwise move[i] contains cell to which  
-    // snake or ladder at i takes to. 
+{  
     static int getMinDiceThrows(int move[], int n)  // find min dice
     { // move = map / n = array MAX 
     	int count = 0;
@@ -20,25 +13,18 @@ public class StackProblem
         qe.v = 0; 
         qe.dist = 0; 
   
-        // Mark the node 0 as visited and enqueue it. 
         visited[0] = 1; 
         q.add(qe); // init
   
-        // Do a BFS starting from vertex at index 0 
         while (!q.isEmpty())  
         { 
             qe = q.pop();
             System.out.println("remove : " + qe.dist+ "," + qe.v);
             int v = qe.v; 
   
-            // If front vertex is the destination  
-            // vertex, we are done 
             if (v == n - 1)   // goal
             	break; 
             
-            // Otherwise dequeue the front vertex and  
-            // enqueue its adjacent vertices (or cell  
-            // numbers reachable through a dice throw) 
             for (int j = v + 1; j <= (v + 6) && j < n; ++j)  //dice = 1 ~ 6 && j's max = n-1
             { 
                 // If this cell is already visited, then ignore 
@@ -49,11 +35,7 @@ public class StackProblem
                     qentry a = new qentry(); 
                     a.dist = (qe.dist + 1); 
                     visited[j] = 1; 
-                    if (v == n - 1)  // goal
-                        break; 
-                    // Check if there a snake or ladder at 'j' 
-                    // then tail of snake or top of ladder 
-                    // become the adjacent of 'i' 
+
                     if (move[j] != -1)  // first visit
                         a.v = move[j]; 
                     else // already visited
@@ -62,20 +44,13 @@ public class StackProblem
                     count++;
                 } 
             } 
-           // System.out.println("===Queue print===");
-            //System.out.println();
         } 
-        //list 추가한거 출력해보기
-        //print visited
-        // We reach here when 'qe' has last vertex 
-        // return the distance of vertex in 'qe' 
         System.out.println("count = " + count);
         return qe.dist; 
     } 
   
     public static void main(String[] args)  
     { 
-        // Let us construct the board given in above diagram 
         int N = 100; 
         int moves[] = new int[N]; 
         for (int i = 0; i < N; i++)  // init ary to -1
